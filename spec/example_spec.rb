d346e932174d3bfb4080129ddbb0a7df75d4f7ba
daniel holdsworth <danielholdsworth@cci-danholdsworth-k330g.fritz.box>
2021-12-15 09:35:33 +0000

    added rpsec test

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,7 +1,7 @@
-
-RSpec.describe Car do 
-    it 'has a brand' do 
-        car = Car.new('Ford')
-        expect(car.brand).to eq('Ford')
-    end 
-end 
+RSpec.describe User, :type => :model do
+    it "is valid with valid attributes"
+    it "is not valid without a title"
+    it "is not valid without a description"
+    it "is not valid without a start_date"
+    it "is not valid without a end_date"
+  end
