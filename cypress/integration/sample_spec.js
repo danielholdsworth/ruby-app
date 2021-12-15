0c4994606fe66c47a20557bd766655bf00a1e6b1
daniel holdsworth <danielholdsworth@cci-danholdsworth-k330g.fritz.box>
2021-12-15 09:52:19 +0000

    added rspec gem

--- cypress/integration/sample_spec.js
+++
@@ -1,6 +0,0 @@
-describe('My First Test', () => {
-    it("has anchor tags", () => {
-        cy.visit("2020/develop-preview-test");
-        cy.contains("a", "#");
-    })
-})
--- spec/example_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe User, :type => :model do
-  it "is valid with valid attributes"
-  it "is not valid without a email"
-end
