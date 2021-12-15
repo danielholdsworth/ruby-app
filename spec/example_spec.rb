fdbd16c7157935fe32d80429feaf0db02021bfb4
daniel holdsworth <danielholdsworth@cci-danholdsworth-k330g.fritz.box>
2021-12-15 09:46:18 +0000

    added rspec gem

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-    it "is valid with valid attributes" do
-        expect(User.new).to be_valid
-      end
-  end
+  it "is valid with valid attributes"
+  it "is not valid without a email"
+end
