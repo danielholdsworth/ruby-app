424d3a2ef7196a5bb381c686c8b4f5890e3eb6a5
daniel holdsworth <danielholdsworth@cci-danholdsworth-k330g.fritz.box>
2021-12-15 09:37:59 +0000

    added more to rspec test

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,7 +1,7 @@
+require 'rails_helper'
+
 RSpec.describe User, :type => :model do
-    it "is valid with valid attributes"
-    it "is not valid without a title"
-    it "is not valid without a description"
-    it "is not valid without a start_date"
-    it "is not valid without a end_date"
+    it "is valid with valid attributes" do
+        expect(User.new).to be_valid
+      end
   end
