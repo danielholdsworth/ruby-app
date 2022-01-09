4c1e30cb9423d9d8209666a37f5acf1101027890
daniel holdsworth <danielholdsworth@cci-danholdsworth-k330g.fritz.box>
2022-01-09 17:19:21 +0000

    added comment to test

--- test/controllers/welcome_test.rb
+++ test/controllers/welcome_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+#test to see if CirclCI Ruby App appears on the home page.
 
 class BlogFlowTest < ActionDispatch::IntegrationTest
   test 'can see the welcome page' do
