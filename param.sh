419d39fb44ec035739148c4395229315b4c45f83
Dan Holdsworth <dan.holdsworth@circleci.com>
2022-04-05 11:30:29 +0100

    testing params [ci minor]

--- param.sh
+++ param.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 GIT_COMMIT_MSG=git log --format=oneline -n 1 $CIRCLE_SHA1
 CI_COMMIT_LABEL=[[ "$GIT_COMMIT_MSG" =~ ^.*\[(major|minor|patch)\ ci\]|\[ci\ (major|minor|patch)\].*$ ]] && echo ${BASH_REMATCH[1]}${BASH_REMATCH[2]}
