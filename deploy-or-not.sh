fec28426d0bd5ef4d56ca116debc4c4ef20c904a
Dan Holdsworth <dan.holdsworth@circleci.com>
2022-04-07 09:48:57 +0100

    foo bar [deploy]

--- deploy-or-not.sh
+++ deploy-or-not.sh
@@ -5,9 +5,9 @@ GIT_COMMIT_MSG=$(git log --pretty=oneline -n 1)
 if [[ $GIT_COMMIT_MSG == *"[deploy]"* ]]; then
   echo "Deploy Requested!"
   curl --request POST \
-  --url https://circleci.com/api/v2/project/gh/danielholdsworth/ruby-app/pipeline \
+  --url "https://circleci.com/api/v2/project/gh/$ORG/$PROJECT/pipeline" \
   --header "Circle-Token: $CIRCLE_TOKEN" \
   --header 'content-type: application/json' #\
-  #--data "{'parameters':{"$PARAM":"$BOOL"}}"
+  #--data "{'parameters':{$PARAM:$BOOL}}"
 fi
 
