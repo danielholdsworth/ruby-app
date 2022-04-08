35e6b44ae256cf97daf56e482a999994e741578b
Dan Holdsworth <dan.holdsworth@circleci.com>
2022-04-08 10:01:02 +0100

    added my orb

--- deploy-or-not.sh
+++ deploy-or-not.sh
@@ -2,7 +2,7 @@
 
 GIT_COMMIT_MSG=$(git log --pretty=oneline -n 1)
 
-if [[ $GIT_COMMIT_MSG == *"[deploy]"* ]]; then
+if [[ $GIT_COMMIT_MSG == *"[$PARAM]"* ]]; then
   echo "Deploy Requested!"
   curl --request POST \
   --url "https://circleci.com/api/v2/project/gh/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pipeline" \
